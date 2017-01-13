0290eb2fbee4125d842ab30200ee1b1e9f402986
BJ Pate <bpate@comparionanalytics.com>
2017-01-13 08:23:33 -0500

    Initial Setup of Localized Library

---
+++ ACM.Common/ACM.Common/LoggingService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ACM.Common
+{
+    public class LoggingService
+    {
+        public static void WriteToFile<T>(List<T> changedItems)
+            where T : ILoggable
+        {
+            foreach(var item in changedItems)
+            {
+                Console.WriteLine(item.Log());
+            }
+        }
+    }
+}
---
+++ ACM.Common/ACM.Common/StringHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ACM.Common
+{
+    public static class StringHandler
+    {
+        public static string InsertSpaces(this string source)
+        {
+            string results = string.Empty;
+            if (!String.IsNullOrWhiteSpace(source))
+            {
+                foreach(char letter in source)
+                {
+                    if (char.IsUpper(letter) )
+                    {
+                        results = results.Trim();
+                        results += " ";
+                    }
+                    results += letter;
+                }
+            }
+            return results.Trim();
+        }
+    }
+}
